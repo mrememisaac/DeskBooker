2be76381a1c8c3b3e90064a84c15d04284110f5a
Emem Isaac <me@ememisaac.com>
2021-03-10 22:00:32 +0100

    This closes issue #3 Desk bookings should be saved

---
+++ DeskBooker.Core/Processor/DeskBookingProcessor.cs
@@ -0,0 +1,38 @@
+﻿using DeskBooker.Data;
+using DeskBooker.Entities;
+using System;
+
+namespace DeskBooker.Core
+{
+    public class DeskBookingProcessor
+    {
+        private readonly IDeskBookingRepository deskBookingRepository;
+
+        public DeskBookingProcessor()
+        {
+        }
+
+        public DeskBookingProcessor(IDeskBookingRepository deskBookingRepository)
+        {
+            this.deskBookingRepository = deskBookingRepository;
+        }
+
+        public BookDeskRequestResult BookDesk(BookDeskRequest request)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            deskBookingRepository.Save(Create<DeskBooking>(request));
+            return Create<BookDeskRequestResult>(request);
+        }
+
+        private static T Create<T>(BookDeskRequest request) where T : DeskBookingBase, new()
+        {
+            return new T
+            {
+                Date = request.Date,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName
+            };
+        }
+    }
+}
