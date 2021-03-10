0ca43dfab7eb131c0f5fd137b5c8cd548633ac87
Emem Isaac <me@ememisaac.com>
2021-03-10 22:36:21 +0100

    This closes #4 Check if a desk is available before saving

--- DeskBooker.Core/Processor/DeskBookingProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingProcessor.cs
@@ -1,26 +1,32 @@
 ﻿using DeskBooker.Data;
 using DeskBooker.Entities;
 using System;
+using System.Linq;
 
 namespace DeskBooker.Core
 {
     public class DeskBookingProcessor
     {
         private readonly IDeskBookingRepository deskBookingRepository;
+        private readonly IDeskRepository deskRepository;
 
         public DeskBookingProcessor()
         {
         }
 
-        public DeskBookingProcessor(IDeskBookingRepository deskBookingRepository)
+        public DeskBookingProcessor(IDeskBookingRepository deskBookingRepository, IDeskRepository deskRepository)
         {
             this.deskBookingRepository = deskBookingRepository;
+            this.deskRepository = deskRepository;
         }
 
         public BookDeskRequestResult BookDesk(BookDeskRequest request)
         {
             if (request == null) throw new ArgumentNullException(nameof(request));
-            deskBookingRepository.Save(Create<DeskBooking>(request));
+            if(deskRepository.GetAvailableDesks(request.Date).Count() > 0)
+            {
+                deskBookingRepository.Save(Create<DeskBooking>(request));
+            }
             return Create<BookDeskRequestResult>(request);
         }
 
